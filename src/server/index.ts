250ce983967c2640499e50bb67ab56204b527779
Rees <reesmorriscontact@gmail.com>
2022-01-17 22:40:50 +0000

    Update dependencies

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,26 +1,21 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import next from 'next';
+import { ServerResponse } from 'http';
 
+const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
-const port = process.env.PORT || 3000;
 
-(async () => {
-  try {
-    await app.prepare();
-    const server = express();
+app.prepare().then(() => {
+  const server = express();
 
-    server.all('*', (req: Request, res: Response) => {
-      return handle(req, res);
-    });
+  server.all('*', (req, res) => {
+    return handle(req, res as ServerResponse);
+  });
 
-    server.listen(port, (err?: Error) => {
-      if (err) throw err;
-      console.log(`🚀 Ready on http://localhost:${port}`);
-    });
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
-  }
-})();
+  server.listen(port, (err?: Error) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
